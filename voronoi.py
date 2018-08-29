898776071bd51c5320ad9b66c7db3a621ed1d6ec
omnitrogen <felixgif@gmail.com>
2018-08-30 00:12:00 +0200

    not working. Points disappear

--- voronoi.py
+++ voronoi.py
@@ -1,15 +1,15 @@
 import sys
 import math
 import random
+import numpy as np
 import pygame
 from pygame.locals import *
 
 pygame.init()
 surf = pygame.display.set_mode((800, 600))
-surf.fill((255, 255, 255))
+surf.fill((10, 10, 10))
 
-pointsList = [] # associer a chaque point une couleur, puis calculer pour chaque pixel la distance euclidienne au points (math.sqrt(((a1-b1)**2) + (a2-b2)**2))
-graphicPointsList = []
+points = [] # associer a chaque point une couleur, puis calculer pour chaque pixel la distance euclidienne au points (math.sqrt(((a1-b1)**2) + (a2-b2)**2))
 pix = pygame.PixelArray(surf)
 
 pygame.display.set_caption('Voronoi Diagram')
@@ -17,20 +17,25 @@ pygame.display.set_caption('Voronoi Diagram')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-            x, y = pygame.mouse.get_pos()
-            graphicPointsList.append([surf, (255, 0, 255), (x, y), 5, 1])
-            pointsList.append([x, y, (random.randint(1, 255), random.randint(1, 255), random.randint(1, 255))])
-            if pointsList.__len__() == 1:
-                pix[...] = pointsList[0][2]
+            posx, posy = pygame.mouse.get_pos()
+            points.append([np.array([posx, posy]), (random.randint(1, 254), random.randint(1, 254), random.randint(1, 254))])
+            pygame.draw.circle(surf, (255, 255, 255), (posx, posy), 3, 3)
+
+            if points.__len__() == 1:
+                for x, y in [(x, y) for x in range(surf.get_size()[0]) for y in range(surf.get_size()[1])]:
+                    if pix[x, y] == surf.map_rgb((255, 255, 255)):
+                        pix[x, y] = (255, 255, 255)
+                    else:
+                        pix[x, y] = points[0][1]
             else:
-                pix = pygame.PixelArray(surf)
-                for x, y in [(x, y) for x in [i for i in range(surf.get_size()[0])] for y in [i for i in range(surf.get_size()[1])]]:
-                    pix[x][y] = (200, 0, 0)
-                del pix
-            for elt in graphicPointsList:
-                pygame.draw.circle(elt[0], elt[1], elt[2], elt[3], elt[4])
+                for x, y in [(x, y) for x in range(surf.get_size()[0]) for y in range(surf.get_size()[1])]:
+                    if pix[x, y] == surf.map_rgb((255, 255, 255)):
+                        pass
+                    else:
+                        pix[x, y] = 0
 
         if event.type == QUIT:
+            del pix
             pygame.quit()
             sys.exit()
     pygame.display.update()
